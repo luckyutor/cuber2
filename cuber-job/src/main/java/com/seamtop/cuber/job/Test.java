ce3921fbd6b3e702b319d6570e935db8dfec6b75
luckyutor <better_feng@126.com>
2015-08-09 13:46:05 +0800

    client初始化

---
+++ cuber-job/src/main/java/com/seamtop/cuber/job/Test.java
@@ -0,0 +1,7 @@
+package com.seamtop.cuber.job;
+
+/**
+ * Created by feng on 2015/8/9.
+ */
+public class Test {
+}
---
+++ cuber-storage/src/main/java/com/seamtop/cuber/storage/Test.java
@@ -0,0 +1,7 @@
+package com.seamtop.cuber.storage;
+
+/**
+ * Created by feng on 2015/8/9.
+ */
+public class Test {
+}
