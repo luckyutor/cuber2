daf4ab406c4dfa6a314365d4d7b3aca40a23cb27
zongjunfeng <zongjunfeng@sohu-inc.com>
2015-08-10 13:10:33 +0800

    修改。

--- cuber-storage/src/test/java/com/seamtop/CuberSplitterTest.java
+++ cuber-storage/src/test/java/com/seamtop/CuberSplitterTest.java
@@ -1,6 +1,7 @@
 package com.seamtop;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.seamtop.cuber.storage.kafka.CuberSplitter;
 
@@ -15,20 +16,18 @@ import java.util.HashMap;
 public class CuberSplitterTest {
     @Test
     public void testCuberSplitter(){
-        String res = "{\"task\":{\"taskData\":{\"sale_price\":\"120000\",\"mileage\":\"1000\",\"province_id\":\"110000\",\"brand_id\":\"191\",\"first_license_date\":\"1439177426993\",\"trimm_id\":\"3700\",\"id\":\"1\",\"city_id\":\"111000\",\"flag\":\"0\",\"create_date\":\"1439177426993\",\"color\":\"ab\",\"flag_source\":\"0\",\"model_id\":\"2344\",\"dealer_id\":\"110000\"},\"taskId\":143917742711415,\"taskTime\":1439177427114,\"taskType\":1}}";
+        String res = "{\"task\":{\"messageList\":[{\"msgData\":{\"sale_price\":\"120000\",\"mileage\":\"1000\",\"province_id\":\"110000\",\"brand_id\":\"191\",\"first_license_date\":\"1439182827296\",\"trimm_id\":\"3700\",\"id\":\"1\",\"city_id\":\"111000\",\"flag\":\"0\",\"create_date\":\"1439182827296\",\"color\":\"黑色\",\"flag_source\":\"0\",\"model_id\":\"2344\",\"dealer_id\":\"110000\"},\"msgType\":1}],\"taskId\":143918282740457,\"taskSource\":1,\"taskTime\":1439182827404}}";
         JSONObject taskObject = JSON.parseObject(res);
-        System.out.println("taskObject"+taskObject);
         JSONObject jsonObject = (JSONObject)taskObject.get("task");
         int taskSource = (Integer)jsonObject.get("taskSource");
-        System.out.println(taskSource);
-        JSONObject taskDataObject = (JSONObject)jsonObject.get("taskData");
-        System.out.println("taskSource:"+taskSource + " map:"+taskDataObject.get("sale_price"));
-        System.out.println(taskSource);
+        JSONArray msgArray = (JSONArray)jsonObject.get("messageList");
+        System.out.println("msgArray:"+msgArray);
         switch (taskSource){
             case 1://为客户端API接口
 
                 break;
-            case 2:
+            default:
+                break;
 
         }
     }
