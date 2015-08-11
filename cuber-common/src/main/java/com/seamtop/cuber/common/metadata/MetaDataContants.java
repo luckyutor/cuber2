25414f640bd6334cabf8710aab6fc9f0e19a57ff
zongjunfeng <zongjunfeng@sohu-inc.com>
2015-08-11 11:46:49 +0800

    修改。

---
+++ cuber-common/src/main/java/com/seamtop/cuber/common/metadata/MetaDataContants.java
@@ -0,0 +1,15 @@
+package com.seamtop.cuber.common.metadata;
+
+import java.util.List;
+
+/**
+ * Created by zongjunfeng on 2015/8/11.
+ */
+public enum  MetaDataContants {
+
+    INSTANCE;
+
+    public static List<TableMetaData> metaDataList;
+
+
+}
---
+++ cuber-common/src/main/java/com/seamtop/cuber/common/metadata/ValueTypeContants.java
@@ -0,0 +1,19 @@
+package com.seamtop.cuber.common.metadata;
+
+/**
+ * Created by zongjunfeng on 2015/8/11.
+ */
+public class ValueTypeContants {
+
+    public static int VALUE_TYPE_STRING = 1;
+
+    public static int VALUE_TYPE_INT = 2;
+
+    public static int VALUE_TYPE_FLOAT = 3;
+
+    public static int VALUE_TYPE_DOUBLE = 4;
+
+    public static int VALUE_TYPE_LONG = 5;
+
+    public static int VALUE_TYPE_DATE = 6;
+}
