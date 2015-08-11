c31b2f070ad0ba238a2653de89158b7fd5d13cfe
zongjunfeng <zongjunfeng@sohu-inc.com>
2015-08-11 18:04:27 +0800

    修改。

---
+++ cuber-common/src/main/java/com/seamtop/cuber/common/util/ParamsUtil.java
@@ -0,0 +1,20 @@
+package com.seamtop.cuber.common.util;
+
+import com.seamtop.cuber.common.metadata.ValueTypeContants;
+
+/**
+ * Created by zongjunfeng on 2015/8/11.
+ */
+public class ParamsUtil {
+
+    public static boolean caliParams(int valueType,String str){
+        switch (valueType){
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+        return false;
+    }
+
+}
