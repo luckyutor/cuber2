9a80678195313226b0d23f08d29209e21cd16f4b
zongjunfeng <zongjunfeng@sohu-inc.com>
2015-08-12 11:41:55 +0800

    修改。

---
+++ cuber-storage/src/main/java/com/seamtop/cuber/storage/kafka/TransData.java
@@ -0,0 +1,44 @@
+package com.seamtop.cuber.storage.kafka;
+
+import org.apache.hadoop.hbase.client.Put;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by zongjunfeng on 2015/8/12.
+ */
+public class TransData implements Serializable {
+
+    private static final long serialVersionUID = -2381284536384630295L;
+
+    private String familyName;
+
+    private String columnName;
+
+    private String value;
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
