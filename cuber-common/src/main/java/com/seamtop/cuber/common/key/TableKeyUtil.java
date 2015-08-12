74290b6f90815c74bc52298d339b3843ecbb6a93
zongjunfeng <zongjunfeng@sohu-inc.com>
2015-08-12 18:22:59 +0800

    修改。

---
+++ cuber-common/src/main/java/com/seamtop/cuber/common/key/TableKeyUtil.java
@@ -0,0 +1,12 @@
+package com.seamtop.cuber.common.key;
+
+/**
+ * Created by zongjunfeng on 2015/8/11.
+ */
+public class TableKeyUtil {
+
+    public int getRowKey(String type){
+        return 0;
+    }
+
+}
